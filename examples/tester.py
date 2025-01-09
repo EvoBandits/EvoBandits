b00553f59b28320ea0d9a44b55bc25e49afa4965
timo <22354443+tnkuehne@users.noreply.github.com>
2025-01-09 23:54:00 +0100

    bounds + new names

--- examples/tester.py
+++ examples/tester.py
@@ -1,8 +1,8 @@
-from pygmab import call_python_function_with_chosen_arg
+from pygmab import optimizer as gmab
 
 def test_function(number: list):
-    # list items ** 2 and then sum
     return sum([i ** 2 for i in number])
 
 if __name__ == '__main__':
-    print(call_python_function_with_chosen_arg(test_function))
+    bounds = [[-10, 10], [-10, 10]]
+    print(gmab(test_function, bounds))
--- pygmab/src/lib.rs
+++ pygmab/src/lib.rs
@@ -2,7 +2,8 @@ use pyo3::prelude::*;
 use pyo3::types::PyList;
 
 #[pyfunction]
-fn call_python_function_with_chosen_arg(py: Python, py_func: PyObject) -> PyResult<i64> {
+fn optimizer(py: Python, py_func: PyObject, bounds: Vec<Vec<i32>>) -> PyResult<i64> {
+    println!("{:?}", bounds);
     let elements: Vec<i32> = vec![0, 1, 2, 3, 4, 0];
     let list = PyList::new(py, elements)?;
 
@@ -15,6 +16,6 @@ fn call_python_function_with_chosen_arg(py: Python, py_func: PyObject) -> PyResu
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pygmab(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(call_python_function_with_chosen_arg, m)?)?;
+    m.add_function(wrap_pyfunction!(optimizer, m)?)?;
     Ok(())
 }
