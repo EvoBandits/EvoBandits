c3ba83ac7a72a8e5866e286b51cd71f056443d44
fwuermseher <f.wuermseher@t-online.de>
2025-01-26 14:25:10 +0100

    Adds docstrings

--- pygmab/gmab/study/study.py
+++ pygmab/gmab/study/study.py
@@ -4,25 +4,58 @@ from gmab import Gmab
 
 
 class Study:
-    def __init__(self, study_name: str | None = None) -> None:
-        self.study_name: str | None = study_name
+    """A Study corresponds to an optimization task, i.e. a set of trials.
+
+    This objecht provides interfaces to optimize an objective within its bounds,
+    and to set/get attributes of the study itself that are user-defined.
+
+    Note that the direct use of this constructor is not recommended.
+    To create a study, use :func:`~gmab.create_study`.
+
+    """
+
+    def __init__(self) -> None:
         self._best_trial: dict | None = None
 
     @property
     def best_trial(self) -> dict:
+        """Return the parameters of the best trial in the study.
+
+        Returns:
+            A dictionary containing parameters of the best trial.
+
+        """
         if not self._best_trial:
             raise RuntimeError("best_trial is not available yet. Run study.optimize().")
         return self._best_trial
 
     def optimize(
         self,
-        objective: Callable,
+        func: Callable,
         bounds: list[tuple],
         n_simulations: int,
     ) -> None:
-        gmab = Gmab(objective, bounds)
+        """Optimize an objective function.
+
+        Optimization is done by choosing a suitable set of hyperparmeter values within
+        given ``bounds``
+
+        The optimization trial will be stopped after ``n_simulations`` of the
+        :func:`func`.
+
+        Args:
+            func:
+                A callable that implements the objective function.
+            bounds:
+                A list of of tuples that define the bounds for each decision variable.
+            n_simulations:
+                The number of simulations per trial. A trial will continue until the
+                number of elapsed simulations reaches `n_simulations`.
+        """
+        gmab = Gmab(func, bounds)
         self._best_trial = gmab.optimize(n_simulations)
 
 
 def create_study(study_name: str | None = None) -> Study:
+    """Create a new :class:`~gmab.study.Study`."""
     return Study(study_name)
