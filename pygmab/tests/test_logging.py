3b76bb5d063d20ac749a95904795e8a1546fe337
fwuermseher <f.wuermseher@t-online.de>
2025-01-28 09:33:00 +0100

    mvoves test_logging to appropriate dir

--- pygmab/tests/test_logging.py
+++ pygmab/tests/test_logging.py
@@ -7,4 +7,4 @@ def test_get_logger(caplog: _pytest.logging.LogCaptureFixture) -> None:
     _logger = logging.get_logger("gmab.foo")
     with caplog.at_level(logging.INFO, logger="gmab.foo"):
         _logger.info("hello")
-    assert "hello" in caplog.text
+        assert "hello" in caplog.text
