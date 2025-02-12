8ae0721c6ee8cd0b12d7a5ddead4dd79e9147c93
fwuermseher <f.wuermseher@t-online.de>
2025-02-12 18:28:28 +0100

    docstrings

--- pygmab/python/gmab/bounds/bounds.py
+++ pygmab/python/gmab/bounds/bounds.py
@@ -1,14 +1,36 @@
 class Bounds:
+    """The Bounds module manages the user-configured boundaries for gmab.
+
+    This object provides interfaces to set the bounds that will be suggested
+    for the optimization.
+    """
+
     def __init__(self) -> None:
         self.low = []
         self.high = []
 
     @property
-    def as_tuples(self) -> list:
+    def internal(self) -> list[tuple]:
+        """Creates and returns the internal bounds for gmab.
+
+        Returns:
+            list[tuple]: A list of tuples, containaing a (low, high) pair for each bound.
+        """
         as_tuple = [(self.low[idx], self.high[idx]) for idx in range(len(self.low))]
         return as_tuple
 
     def suggest_int(self, low: int, high: int, size: int = 1) -> None:
+        """Adds an integer decision parameter to the bounds.
+
+        Args:
+            low (int): The lower bound of the parameter.
+            high (int): The upper bound of the parameter.
+            size (int, optional): Length of the decision vector, defaults to 1.
+
+        Raises:
+            TypeError: If low, high, or size are not integers.
+            ValueError: If high <= low or size non-positive.
+        """
         if not all(isinstance(var, int) for var in [low, high, size]):
             raise TypeError("low, high, size must be integers when suggesting an int.")
         if high <= low:
--- pygmab/tests/bounds_tests.py/test_bounds.py
+++ pygmab/tests/bounds_tests.py/test_bounds.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "low, high, kwargs, exp_tuples",
+    "low, high, kwargs, exp_internal",
     [
         pytest.param(0, 1, {}, [(0, 1)]),
         pytest.param(0, 5, {"size": 1}, [(0, 5)]),
@@ -15,7 +15,7 @@ import pytest
         pytest.param(0, 1, {"size": 0}, [], marks=pytest.mark.xfail(raises=ValueError)),
     ],
 )
-def test_suggest_int(low, high, kwargs, exp_tuples):
+def test_suggest_int(low, high, kwargs, exp_internal):
     bounds = gmab.Bounds()
     bounds.suggest_int(low=low, high=high, **kwargs)
-    assert bounds.as_tuples == exp_tuples
+    assert bounds.internal == exp_internal
