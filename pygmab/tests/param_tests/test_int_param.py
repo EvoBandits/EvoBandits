0984ad3af5186c1941053e2c0fc90b039f1a71c3
fwuermseher <f.wuermseher@t-online.de>
2025-02-21 13:09:18 +0100

    adds unittest for int with steps

--- pygmab/tests/param_tests/test_int_param.py
+++ pygmab/tests/param_tests/test_int_param.py
@@ -12,11 +12,14 @@ xValueErr = pytest.mark.xfail(raises=ValueError)
     [
         pytest.param(0, 1, {}, id="base"),
         pytest.param(0, 1, {"size": 2}, id="base_vector"),
+        pytest.param(0, 4, {"step": 2}, id="base_stepped"),
         pytest.param(0.0, 1, {}, marks=xTypeErr, id="f_type_low"),
         pytest.param(0, 1.0, {}, marks=xTypeErr, id="f_type_high"),
         pytest.param(0, 1, {"size": 2.0}, marks=xTypeErr, id="f_type_size"),
+        pytest.param(0, 4, {"step": 2.0}, marks=xTypeErr, id="f_type_step"),
         pytest.param(0, 0, {}, marks=xValueErr, id="f_value_low_high"),
         pytest.param(0, 1, {"size": 0}, marks=xValueErr, id="f_value_size"),
+        pytest.param(0, 4, {"step": 0}, marks=xValueErr, id="f_value_step"),
     ],
 )
 def test_suggest_int(low, high, kwargs):
@@ -27,13 +30,15 @@ def test_suggest_int(low, high, kwargs):
 
 
 @pytest.mark.parametrize(
-    "low, high, kwargs, expected_bounds",
+    "low, high, kwargs, exp_bounds, numbers, exp_mapping",
     [
-        pytest.param(0, 1, {}, [(0, 1)], id="base"),
-        pytest.param(0, 1, {"size": 2}, [(0, 1), (0, 1)], id="base_vector"),
+        pytest.param(0, 1, {}, [(0, 1)], [1], [1], id="base"),
+        pytest.param(0, 1, {"size": 2}, [(0, 1), (0, 1)], [1, 1], [1, 1], id="vector"),
+        pytest.param(0, 3, {"step": 3}, [(0, 1)], [1], [3], id="step"),
+        # pytest.param(0, 4, {"step": 3}, [(0, 2)], [2], [4], id="step_edge_case"),
     ],
 )
-def test_int_param(low, high, kwargs, expected_bounds):
+def test_int_param(low, high, kwargs, exp_bounds, numbers, exp_mapping):
     param = IntParam(low, high, **kwargs)
-    bounds = param.bounds
-    assert bounds == expected_bounds
+    assert param.bounds == exp_bounds
+    assert param.map(numbers) == exp_mapping
