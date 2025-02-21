963684fa263b0e6f79e64010a14aaea08c7f05e3
fwuermseher <f.wuermseher@t-online.de>
2025-02-21 10:56:03 +0100

    fix unittest with patch

--- pygmab/tests/param_tests/test_int_param.py
+++ pygmab/tests/param_tests/test_int_param.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from gmab.params import IntParam, suggest_int
 
@@ -6,29 +8,32 @@ xValueErr = pytest.mark.xfail(raises=ValueError)
 
 
 @pytest.mark.parametrize(
-    "low, high, kwargs, expected_param",
+    "low, high, kwargs",
     [
-        pytest.param(0, 1, {}, IntParam(0, 1), id="base"),
-        pytest.param(0, 1, {"size": 2}, IntParam(0, 1, 2), id="base_vector"),
-        pytest.param(0.0, 1, {}, IntParam(0, 1), marks=xTypeErr, id="f_type_low"),
-        pytest.param(0, 1.0, {}, IntParam(0, 1), marks=xTypeErr, id="f_type_high"),
-        pytest.param(0, 1, {"size": 2.0}, IntParam(0, 1, 2), marks=xTypeErr, id="f_type_size"),
-        pytest.param(0, 0, {}, None, marks=xValueErr, id="f_value_low_high"),
-        pytest.param(0, 1, {"size": 0}, None, marks=xValueErr, id="f_value_size"),
+        pytest.param(0, 1, {}, id="base"),
+        pytest.param(0, 1, {"size": 2}, id="base_vector"),
+        pytest.param(0.0, 1, {}, marks=xTypeErr, id="f_type_low"),
+        pytest.param(0, 1.0, {}, marks=xTypeErr, id="f_type_high"),
+        pytest.param(0, 1, {"size": 2.0}, marks=xTypeErr, id="f_type_size"),
+        pytest.param(0, 0, {}, marks=xValueErr, id="f_value_low_high"),
+        pytest.param(0, 1, {"size": 0}, marks=xValueErr, id="f_value_size"),
     ],
 )
-def test_suggest_int(low, high, kwargs, expected_param):
-    param = suggest_int(low, high, **kwargs)
-    assert param is expected_param
+def test_suggest_int(low, high, kwargs):
+    with patch("gmab.params.int_param.IntParam") as IntParam:
+        _ = suggest_int(low, high, **kwargs)
+        size = kwargs.get("size", 1)
+        IntParam.assert_called_once_with(low, high, size)
 
 
 @pytest.mark.parametrize(
-    "param, expected_bounds",
+    "low, high, kwargs, expected_bounds",
     [
-        pytest.param(suggest_int(0, 1), [(0, 1)], id="base"),
-        pytest.param(suggest_int(0, 1, size=2), [(0, 1), (0, 1)], id="base_vector"),
+        pytest.param(0, 1, {}, [(0, 1)], id="base"),
+        pytest.param(0, 1, {"size": 2}, [(0, 1), (0, 1)], id="base_vector"),
     ],
 )
-def test_int_param_bounds(param, expected_bounds):
+def test_int_param(low, high, kwargs, expected_bounds):
+    param = IntParam(low, high, **kwargs)
     bounds = param.bounds
     assert bounds == expected_bounds
