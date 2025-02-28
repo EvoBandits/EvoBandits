e4025330acc3c4c5613e5ec1e3a0676daffd0ddc
fwuermseher <f.wuermseher@t-online.de>
2025-02-28 14:31:12 +0100

    unittest with parametrization of error cases

--- pygmab/tests/param_tests/test_int_param.py
+++ pygmab/tests/param_tests/test_int_param.py
@@ -1,26 +1,28 @@
+from contextlib import nullcontext
+
 import pytest
 from gmab.params import IntParam
 
-xValueErr = pytest.mark.xfail(raises=ValueError)
+pytest.raises(ValueError)
+
+test_data = [
+    pytest.param(0, 1, {}, [(0, 1)], [1], 1, nullcontext(), id="base"),
+    pytest.param(0, 1, {"size": 2}, [(0, 1), (0, 1)], [1, 1], [1, 1], nullcontext(), id="vector"),
+    pytest.param(3, 6, {"step": 3}, [(3, 4)], [1], 6, nullcontext(), id="step"),
+    pytest.param(0, 4, {"step": 3}, [(0, 2)], [2], 4, nullcontext(), id="step_edge_case"),
+    pytest.param(0.0, 1, {}, None, None, None, pytest.raises(ValueError), id="low_type"),
+    pytest.param(0, 1.0, {}, None, None, None, pytest.raises(ValueError), id="high_type"),
+    pytest.param(0, 0, {}, None, None, None, pytest.raises(ValueError), id="high_value"),
+    pytest.param(0, 1, {"size": 2.0}, None, None, None, pytest.raises(ValueError), id="size_type"),
+    pytest.param(0, 4, {"step": 2.0}, None, None, None, pytest.raises(ValueError), id="step_type"),
+    pytest.param(0, 1, {"size": 0}, None, None, None, pytest.raises(ValueError), id="size_value"),
+    pytest.param(0, 4, {"step": 0}, None, None, None, pytest.raises(ValueError), id="step_value"),
+]
 
 
-@pytest.mark.parametrize(
-    "low, high, kwargs, exp_bounds, action, exp_value",
-    [
-        pytest.param(0, 1, {}, [(0, 1)], [1], 1, id="base"),
-        pytest.param(0, 1, {"size": 2}, [(0, 1), (0, 1)], [1, 1], [1, 1], id="vector"),
-        pytest.param(3, 6, {"step": 3}, [(3, 4)], [1], 6, id="step"),
-        pytest.param(0, 4, {"step": 3}, [(0, 2)], [2], 4, id="step_edge_case"),
-        pytest.param(0.0, 1, {}, None, None, None, marks=xValueErr, id="f_type_low"),
-        pytest.param(0, 1.0, {}, None, None, None, marks=xValueErr, id="f_type_high"),
-        pytest.param(0, 0, {}, None, None, None, marks=xValueErr, id="f_value_low_high"),
-        pytest.param(0, 1, {"size": 2.0}, None, None, None, marks=xValueErr, id="f_type_size"),
-        pytest.param(0, 4, {"step": 2.0}, None, None, None, marks=xValueErr, id="f_type_step"),
-        pytest.param(0, 1, {"size": 0}, None, None, None, marks=xValueErr, id="f_value_size"),
-        pytest.param(0, 4, {"step": 0}, None, None, None, marks=xValueErr, id="f_value_step"),
-    ],
-)
-def test_int_param(low, high, kwargs, exp_bounds, action, exp_value):
-    param = IntParam(low, high, **kwargs)
-    assert param.bounds == exp_bounds
-    assert param.map_to_value(action) == exp_value
+@pytest.mark.parametrize("low, high, kwargs, exp_bounds, action, value, expectation", test_data)
+def test_int_param(low, high, kwargs, exp_bounds, action, value, expectation):
+    with expectation:
+        param = IntParam(low, high, **kwargs)
+        assert param.bounds == exp_bounds
+        assert param.map_to_value(action) == value
