dea536a6fcc3523cbe7f2d1c239273a65f70269f
timo <22354443+tnkuehne@users.noreply.github.com>
2025-03-03 23:44:28 +0100

    try fix

--- pygmab/python/gmab/__init__.py
+++ pygmab/python/gmab/__init__.py
@@ -1,12 +1,30 @@
-from gmab import logging
 from gmab.gmab import Gmab
+from gmab.logging import (
+    CRITICAL,
+    DEBUG,
+    ERROR,
+    INFO,
+    WARNING,
+    disable,
+    enable,
+    get_logger,
+    set_level,
+)
 from gmab.search import GmabSearchCV
 from gmab.study import Study, create_study
 
 __all__ = [
     "Gmab",
     "GmabSearchCV",
-    "logging",
+    "get_logger",
+    "set_level",
+    "disable",
+    "enable",
+    "CRITICAL",
+    "DEBUG",
+    "ERROR",
+    "INFO",
+    "WARNING",
     "Study",
     "create_study",
 ]
--- pygmab/tests/test_logging.py
+++ pygmab/tests/test_logging.py
@@ -1,9 +1,9 @@
-from gmab import logging
+import gmab
 from pytest import CaptureFixture, LogCaptureFixture
 
 
 def test_get_logger(caplog: LogCaptureFixture) -> None:
-    logger = logging.get_logger("gmab.foo")
+    logger = gmab.get_logger("gmab.foo")
 
     logger.info("hello")
     assert "hello" in caplog.text  # Checks logging with a simple example
@@ -13,24 +13,24 @@ def test_get_logger(caplog: LogCaptureFixture) -> None:
 
 
 def test_set_level(caplog: LogCaptureFixture) -> None:
-    logger = logging.get_logger("gmab.foo")
+    logger = gmab.get_logger("gmab.foo")
 
-    logging.set_level(logging.DEBUG)
+    gmab.set_level(gmab.DEBUG)
     logger.debug("debug_msg")
     assert "debug_msg" in caplog.text  # level is set to DEBUG
 
-    logging.set_level(logging.CRITICAL)
+    gmab.set_level(gmab.CRITICAL)
     logger.error("error_msg")
     assert "error_msg" not in caplog.text  # level is set to CRITICAL
 
 
 def test_disable(capsys: CaptureFixture) -> None:
-    logger = logging.get_logger("gmab.foo")
+    logger = gmab.get_logger("gmab.foo")
 
-    logging.disable()
+    gmab.disable()
     logger.info("hello")
     assert "hello" not in capsys.readouterr().err  # Logging is disabled
 
-    logging.enable()
+    gmab.enable()
     logger.info("bye")
     assert "bye" in capsys.readouterr().err  # Logging is enabled
