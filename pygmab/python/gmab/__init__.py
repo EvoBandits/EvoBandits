5315b31e93300a929cff0fb2af29581e14e3ccfe
fwuermseher <f.wuermseher@t-online.de>
2025-03-12 15:44:34 +0100

    try fix: relative imports

--- pygmab/python/gmab/__init__.py
+++ pygmab/python/gmab/__init__.py
@@ -1,7 +1,8 @@
-from gmab import logging
 from gmab.gmab import Gmab
-from gmab.search import GmabSearchCV
-from gmab.study import Study, create_study
+
+from . import logging
+from .search import GmabSearchCV
+from .study import Study, create_study
 
 __all__ = [
     "Gmab",
