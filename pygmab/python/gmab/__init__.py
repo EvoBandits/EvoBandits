ed398bc8d8db20326ae41207aa52be3d12115018
fwuermseher <f.wuermseher@t-online.de>
2025-03-17 10:08:22 +0100

    revert try fix

--- pygmab/python/gmab/__init__.py
+++ pygmab/python/gmab/__init__.py
@@ -1,8 +1,10 @@
 from gmab import logging
+from gmab.gmab import Gmab
 from gmab.search import GmabSearchCV
 from gmab.study import Study, create_study
 
 __all__ = [
+    "Gmab",
     "GmabSearchCV",
     "logging",
     "Study",
