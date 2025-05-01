d1bb2b81b6d9a721f06af809e27f63e1343db003
fwuermseher <f.wuermseher@t-online.de>
2025-05-01 14:28:05 +0200

    test action_vector decoding

--- py-evobandits/tests/study_tests/test_study_internals.py
+++ py-evobandits/tests/study_tests/test_study_internals.py
@@ -1,5 +1,5 @@
 import pytest
-from evobandits import IntParam
+from evobandits import CategoricalParam, IntParam
 from evobandits.study.study import Study
 
 
@@ -8,7 +8,7 @@ from evobandits.study.study import Study
     [
         [{"a": IntParam(0, 1)}, [(0, 1)]],
         [{"a": IntParam(0, 1, 2)}, [(0, 1), (0, 1)]],
-        [{"a": IntParam(0, 1, 2), "b": IntParam(1, 2)}, [(0, 1), (0, 1), (1, 2)]],
+        [{"a": IntParam(0, 1, 2), "b": CategoricalParam(["x", "y"])}, [(0, 1), (0, 1), (0, 1)]],
     ],
     ids=[
         "one_dimension",
@@ -24,3 +24,30 @@ def test_collect_bounds(params, exp_bounds):
     # Collect bounds and verify result
     bounds = study._collect_bounds()
     assert bounds == exp_bounds
+
+
+@pytest.mark.parametrize(
+    "params, action_vector, exp_solution",
+    [
+        [{"a": IntParam(0, 1)}, [1], {"a": 1}],
+        [{"a": IntParam(0, 1, 3)}, [0, 1, 0], {"a": [0, 1, 0]}],
+        [
+            {"a": IntParam(0, 1, 2), "b": CategoricalParam(["x", "y"])},
+            [0, 1, 0],
+            {"a": [0, 1], "b": "x"},
+        ],
+    ],
+    ids=[
+        "one_dimension",
+        "one_param",
+        "multiple_params",
+    ],
+)
+def test_decode(params, action_vector, exp_solution):
+    # Mock ro patch dependencies
+    study = Study(seed=42)  # with seed to avoid warning logs
+    study.params = params
+
+    # Decode an action vector and verify result
+    solution = study._decode(action_vector)
+    assert solution == exp_solution
